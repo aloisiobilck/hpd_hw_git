ae55bc1554e542f7dc12f5cc1994c4e06f702d5f
Aloisio Bilck <aloisiobilck@gmail.com>
2017-08-30 19:40:25 -0400

    3nd commit - Frutas Amarelas

--- frutas.rb
+++ frutas.rb
@@ -1,7 +1,11 @@
-class Frutas 
+glass Frutas 
 
 	def frutas ()
 	        puts "Caju / Manga / Ameixa / Caqui / Goiaba"
 	end	
+
+	def frutas_amarelas()
+	        puts "Laranja / Banana / Carambola / Melao / Maracuja"
+	end
 
 end 
