d7a2a11555efd9856546b954a6bff32326195c15
Aloisio Bilck <aloisiobilck@gmail.com>
2017-08-30 19:47:18 -0400

    4nd commit - Frutas Verdes

--- frutas.rb
+++ frutas.rb
@@ -3,5 +3,9 @@ class Frutas
 	def frutas ()
 	        puts "Caju / Manga / Ameixa / Caqui / Goiaba"
 	end	
-
-end 
+	
+	def frutas_verdes()
+	        puts " Abacate / Kiwi / Uvas verdes / Mação verde / Melancia "
+	end
+	
+end
